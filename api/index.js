f5b8271e11733aa0450b99529675d61084f89917
cheang120 <babycode.2023@gmail.com>
2024-07-04 21:52:05 +0800

    create test api routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import path from 'path'
+import userRoutes from './routes/user.route.js'
 
 dotenv.config()
 
@@ -23,7 +24,11 @@ app.listen(3000,()=>{
     console.log('Server is running on port 3000!');
 })
 
+app.use('/api/user', userRoutes)
+
+
 app.use(express.static(path.join(__dirname,'/SoapDelight.J/dist')))
 app.get("*", (req,res)=>{
     res.sendFile(path.join(__dirname,'SoapDelight.J','dist','index.html'))
 })
+
