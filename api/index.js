4fecdf64bb252dac995666d7bce67c2c68d9c7e7
cheang120 <babycode.2023@gmail.com>
2024-07-04 17:07:55 +0800

    add pre installation

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import path from 'path'
 
 dotenv.config()
 
@@ -13,9 +14,16 @@ mongoose
         console.log(err);
     })
 
+    const __dirname = path.resolve()
+
 
 const app = express()
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!');
+})
+
+app.use(express.static(path.join(__dirname,'/SoapDelight.J/dist')))
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname,'SoapDelight.J','dist','index.html'))
 })
