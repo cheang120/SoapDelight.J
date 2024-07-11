9f07208611b7efef15dbfd5dd5bc97ed694240a8
cheang120 <babycode.2023@gmail.com>
2024-07-12 07:25:11 +0800

    complete user image upload functionality

--- SoapDelight.J/src/components/DashSidebar.jsx
+++ SoapDelight.J/src/components/DashSidebar.jsx
@@ -45,8 +45,8 @@ export default function DashSidebar () {
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
       <Sidebar.ItemGroup className='flex flex-col gap-1'>
-        <Link to='/dashboard?tab=profile'>
-            <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User"} labelColor='dark'>
+        <Link to='/dashboard?tab=profile' >
+            <Sidebar.Item as='div' active={tab === 'profile'} icon={HiUser} label={"User"} labelColor='dark'>
                 Profile
             </Sidebar.Item>
         </Link>
