873231633e29ea3dedd35c2a71a124cf70e6c928
cheang120 <babycode.2023@gmail.com>
2024-07-18 17:07:30 +0800

    index.js

--- api/index.js
+++ api/index.js
@@ -5,14 +5,12 @@ import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-import cors from 'cors';
-
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log('MongoDb is connected');
+    console.log('Connected to MongoDB');
   })
   .catch((err) => {
     console.log(err);
@@ -22,32 +20,29 @@ const __dirname = path.resolve();
 
 const app = express();
 
-app.use(cors())
 app.use(express.static(path.join(__dirname, '/client/dist')));
-// app.use(express.json());
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+app.use(express.json());
+
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+  console.log('Server listening on port 3000');
 });
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
-// app.use('/api/post', postRoutes);
-// app.use('/api/comment', commentRoutes);
-
-
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/dist/index.html'));
-});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
-  res.status(statusCode).json({
+  return res.status(statusCode).json({
     success: false,
-    statusCode,
     message,
+    statusCode,
   });
 });
