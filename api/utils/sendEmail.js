49ebcdfaa2778c2be2c4bcddd3727384cebd6668
cheang120 <babycode.2023@gmail.com>
2024-07-18 19:11:01 +0800

    sendEmail.js

--- api/utils/sendEmail.js
+++ api/utils/sendEmail.js
@@ -1,8 +1,8 @@
-import  nodemailer from "nodemailer";
-import  hbs from  "nodemailer-express-handlebars";
-import  path  from "path";
+import nodemailer from "nodemailer";
+import hbs from "nodemailer-express-handlebars";
+import path from "path";
 
- const sendEmail = async (
+const sendEmail = async (
   subject,
   send_to,
   sent_from,
@@ -11,52 +11,55 @@ import  path  from "path";
   name,
   link
 ) => {
-  // Create Email Transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: 587,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
+  try {
+    // Create Email Transporter
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: 587,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
 
-  const handlebarOptions = {
-    viewEngine: {
+    const handlebarOptions = {
+      viewEngine: {
+        extName: ".handlebars",
+        partialsDir: path.resolve("./api/views"),
+        defaultLayout: false,
+      },
+      viewPath: path.resolve("./api/views"),
       extName: ".handlebars",
-      partialsDir: path.resolve("./api/views"),
-      defaultLayout: false,
-    },
-    viewPath: path.resolve("./api/views"),
-    extName: ".handlebars",
-  };
+    };
 
-  transporter.use("compile", hbs(handlebarOptions));
+    transporter.use("compile", hbs(handlebarOptions));
 
-  // Options for sending email
-  const options = {
-    from: sent_from,
-    to: send_to,
-    replyTo: reply_to,
-    subject,
-    template,
-    context: {
-      name,
-      link,
-    },
-  };
+    // Options for sending email
+    const options = {
+      from: sent_from,
+      to: send_to,
+      replyTo: reply_to,
+      subject,
+      template,
+      context: {
+        name,
+        link,
+      },
+    };
 
-  // Send Email
-  transporter.sendMail(options, function (err, info) {
-    if (err) {
-      console.log(err);
+    // Send Email
+    let info = await transporter.sendMail(options);
+    console.log('Email sent: ', info);
+  } catch (err) {
+    if (err.responseCode === 535) {
+      console.error('Authentication failed. Please check your SMTP server credentials.');
     } else {
-      console.log(info);
+      console.error('Error occurred: ', err);
     }
-  });
+  }
 };
 
 export default sendEmail;
