106ff0818c2bfd9c44433a82101e59dec6539e3e
cheang120 <babycode.2023@gmail.com>
2024-07-18 22:54:34 +0800

    index.js

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://soapdelight-j.onrender.com"],
     credentials: true,
   })
 );
