fbf4436a5f5d1d784d79bdf254d438ede106a6cf
cheang120 <babycode.2023@gmail.com>
2024-07-18 17:00:15 +0800

    index.js

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ app.use('/api/auth', authRoutes);
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '/client/dist/index.html'));
 });
 
 app.use((err, req, res, next) => {
