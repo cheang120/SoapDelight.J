78e970b7634e6ab87843d2d91361f0048ee25e3b
cheang120 <babycode.2023@gmail.com>
2024-07-18 17:35:31 +0800

    index.js

--- api/index.js
+++ api/index.js
@@ -1,46 +1,82 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import path from 'path'
-import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+import cors from 'cors'
+dotenv.config();
 
-dotenv.config()
+const app = express();
+const __dirname = path.resolve();
+
+
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, '/SoapDelight.J/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'SoapDelight.J', 'dist', 'index.html'));
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(bodyParser.json());
+// app.use(express.json());
+
+// app.use(cookieParser());
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
 
 mongoose
-    .connect(process.env.MONGO)
-    .then(()=>{
-        console.log('MongoDb is connected');
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-
-    const __dirname = path.resolve()
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 
-const app = express()
-app.use(express.json())
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000!');
-})
-
-app.use('/api/user', userRoutes)
-app.use('/api/auth', authRoutes)
-
-app.use((err, req,res,next)=>{
-    const statusCode = err.statusCode || 500
-    const message = err.message || 'Internal Server Error'
-    res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message
-    })
-})
+// const __dirname = path.resolve();
 
 
-app.use(express.static(path.join(__dirname,'/SoapDelight.J/dist')))
-app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname,'SoapDelight.J','dist','index.html'))
-})
+
+// app.use(express.static(path.join(__dirname, '/client/dist')));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// });
+
+
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
+
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
