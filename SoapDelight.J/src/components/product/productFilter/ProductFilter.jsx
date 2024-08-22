54509b4e3438c26ce41246f4f845ddc79573b0c5
cheang120 <babycode.2023@gmail.com>
2024-08-22 23:11:43 +0700

    filter category complete

--- SoapDelight.J/src/components/product/productFilter/ProductFilter.jsx
+++ SoapDelight.J/src/components/product/productFilter/ProductFilter.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./ProductFilter.module.scss";
+import {
+  // FILTER_BY_BRAND,
+  FILTER_BY_CATEGORY,
+  // FILTER_BY_PRICE,
+} from "../../../redux/features/product/filtersSlice";
+
 
 const ProductFilter = () => {
+  const { products, minPrice, maxPrice } = useSelector(
+    (state) => state.product
+  );
+  const [category, setCategory] = useState("All");
+  const [brand, setBrand] = useState("All");
+  const [price, setPrice] = useState([50, 1500]);
+
+  const dispatch = useDispatch();
+
+  const allCategories = [
+    "All",
+    ...new Set(products?.map((product) => product.category)),
+  ];
+
+  const filterProducts = (cat) => {
+    setCategory(cat);
+    dispatch(FILTER_BY_CATEGORY({ products, category: cat }));
+  };
+
+  const filterProductCategory = (cat) => {
+    // console.log(cat);
+    setCategory(cat);
+    dispatch(FILTER_BY_CATEGORY({ products:products, category: cat }));
+    
+  }
+
+  // console.log(allCategories);
+  const allBrands = [
+    "All",
+    ...new Set(products.map((product) => product.brand)),
+  ];
+  // console.log(allBrands);
   return (
-    <div>
-      ProductFilter
+    <div className={styles.filter}>
+      <h4>Categories</h4>
+      <div className={styles.category}>
+        {allCategories.map((cat,index) => {
+          return (
+              <button
+                key={index}
+                type="button"
+                className={`${category}` === cat ? `${styles.active}` : null}
+                onClick={() => filterProductCategory(cat)}
+              >
+                &#8250; {cat}
+              </button>
+
+          )
+      })}
+      </div>
+
     </div>
   )
 }
--- SoapDelight.J/src/redux/features/product/filtersSlice.jsx
+++ SoapDelight.J/src/redux/features/product/filtersSlice.jsx
@@ -50,12 +50,46 @@ const filtersSlice = createSlice({
   
         state.filteredProducts = tempProducts;
       },
+      FILTER_BY_CATEGORY(state, action) {
+        const { products, category } = action.payload;
+        let tempProducts = [];
+        if (category === "All") {
+          tempProducts = products;
+        } else {
+          tempProducts = products.filter(
+            (product) => product.category === category
+          );
+        }
+        state.filteredProducts = tempProducts;
+      },
+      FILTER_BY_BRAND(state, action) {
+        const { products, brand } = action.payload;
+        let tempProducts = [];
+        if (brand === "All") {
+          tempProducts = products;
+        } else {
+          tempProducts = products.filter((product) => product.brand === brand);
+        }
+        state.filteredProducts = tempProducts;
+      },
+      FILTER_BY_PRICE(state, action) {
+        const { products, price } = action.payload;
+        let tempProducts = [];
+        tempProducts = products.filter(
+          (product) => product.price >= price[0] && product.price <= price[1]
+        );
+  
+        state.filteredProducts = tempProducts;
+      },
   }
 });
 
 export const {
     FILTER_BY_SEARCH,
-    SORT_PRODUCTS
+    SORT_PRODUCTS,
+    FILTER_BY_CATEGORY,
+    FILTER_BY_BRAND,
+    FILTER_BY_PRICE,
 } = filtersSlice.actions
 
 export const selectFilteredProducts = (state) => state.filters.filteredProducts;
