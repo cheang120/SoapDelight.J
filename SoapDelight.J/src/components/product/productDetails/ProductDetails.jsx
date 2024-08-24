3fbda0c00910e4ded943d6560161d5f9cd969c49
cheang120 <babycode.2023@gmail.com>
2024-08-24 12:09:24 +0700

    product details complete

--- SoapDelight.J/src/components/product/productDetails/ProductDetails.jsx
+++ SoapDelight.J/src/components/product/productDetails/ProductDetails.jsx
@@ -5,6 +5,12 @@ import styles from "./ProductDetails.module.scss"
 import { getProduct } from "../../../redux/features/product/productSlice";
 import DOMPurify from "dompurify";
 import { Spinner } from "../../Loader";
+import ProductRating from "../productRating/ProductRating.jsx";
+import { toast } from "react-toastify";
+import {
+     calculateAverageRating, 
+    //  getCartQuantityById 
+} from "../../../utils/index.jsx";
 
 
 const ProductDetails = () => {
@@ -33,6 +39,30 @@ const ProductDetails = () => {
         }
         return () => clearInterval(slideInterval);
       }, [imageIndex, slideInterval, product]);
+
+      const averageRating = calculateAverageRating(product?.ratings);
+
+    //   const cart = cartItems.find((cart) => cart._id === id);
+    //   const isCartAdded = cartItems.findIndex((cart) => {
+    //     return cart._id === id;
+    //   });
+
+      const addToCart = (product) => {
+        // dispatch(ADD_TO_CART(product));
+        // dispatch(CALCULATE_TOTAL_QUANTITY());
+        // dispatch(
+        //   saveCartDB({ cartItems: JSON.parse(localStorage.getItem("cartItems")) })
+        // );
+      };
+    
+      const decreaseCart = (product) => {
+        // dispatch(DECREASE_CART(product));
+        // dispatch(CALCULATE_TOTAL_QUANTITY());
+        // dispatch(
+        //   saveCartDB({ cartItems: JSON.parse(localStorage.getItem("cartItems")) })
+        // );
+      };
+
   return (
     <section>
         <div className={`container ${styles.product}`}>
@@ -65,6 +95,95 @@ const ProductDetails = () => {
                                     )
                                 })}
                             </div>
+                        </div>
+                        <div className={styles.content}>
+                            <h3>{product?.name}</h3>
+
+                            <ProductRating
+                                averageRating={averageRating}
+                                // noOfRatings={product?.ratings.length}
+                            />
+                            <div className="--underline"></div>
+                            <div className={styles.property}>
+                                <p>
+                                    <b>Price:</b>
+                                </p>
+                                <p className={styles.price}>{`$${product?.price}`}</p>
+                            </div>
+                            <div className={styles.property}>
+                                <p>
+                                    <b>SKU:</b>
+                                </p>
+                                <p>{product?.sku}</p>
+                            </div>
+                            <div className={styles.property}>
+                                <p>
+                                    <b>Category: </b>
+                                </p>
+                                <p>{product?.category}</p>
+                            </div>
+                            <div className={styles.property}>
+                                <p>
+                                    <b>Brand: </b>
+                                </p>
+                                <p>{product?.brand}</p>
+                            </div>
+                            <div className={styles.property}>
+                                <p>
+                                    <b>Color: </b>
+                                </p>
+                                <p>{product?.color}</p>
+                            </div>
+                            <div className={styles.property}>
+                                <p>
+                                    <b>Quantity in stock: </b>
+                                </p>
+                                <p>{product?.quantity}</p>
+                            </div>
+                            <div className={styles.property}>
+                                <p>
+                                    <b>Sold: </b>
+                                </p>
+                                <p>{product?.sold}</p>
+                            </div>
+                            <div className={styles.count}>
+
+                            </div>
+                            <div className="--flex-start">
+                                {product?.quantity > 0 ? (
+                                    <button
+                                        className="--btn --btn-primary"
+                                        onClick={() => addToCart(product)}
+                                    >
+                                    ADD TO CART
+                                    </button>
+                                ) : (
+                                    <button
+                                        className="--btn --btn-red"
+                                        onClick={() =>
+                                            toast.error("Sorry, Product is out of stock")
+                                        }
+                                    >
+                                    Out Of Stock
+                                    </button>
+                                )}
+                                <button
+                                    className="--btn --btn-danger"
+                                    onClick={() => addWishlist(product)}
+                                >
+                                    ADD TO WISHLIST
+                                </button>
+                            </div>
+                            <div className="--underline"></div>
+                            <p>
+                            <b>Product Description:</b>
+                            </p>
+                            <div
+                                dangerouslySetInnerHTML={{
+                                    __html: DOMPurify.sanitize(product?.description),
+                                }}
+                            ></div>
+                            <div className="--underline"></div>
                         </div>
                     </div>
                 </>
