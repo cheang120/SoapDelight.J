de659262fae9e477087117b4e0838cfdb9f2fea5
cheang120 <babycode.2023@gmail.com>
2024-09-03 23:08:24 +0800

    checkout form part 1

--- SoapDelight.J/src/components/checkout/checkoutForm/CheckoutForm.jsx
+++ SoapDelight.J/src/components/checkout/checkoutForm/CheckoutForm.jsx
@@ -1,47 +1,85 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./CheckoutForm.module.scss"
 import {
   PaymentElement,
   useStripe,
   useElements
 } from "@stripe/react-stripe-js";
+import Card from "../../card/Card";
+import CheckoutSummary from "../checkoutSummary/CheckoutSummary";
 
 export default function CheckoutForm({dpmCheckerLink}) {
   const stripe = useStripe();
   const elements = useElements();
 
+  const [email, setEmail] = useState("")
   const [message, setMessage] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    if (!stripe) {
+      return;
+    }
+
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+
+    if (!clientSecret) {
+      return;
+    }
+  }, [stripe]);
+
+  const saveOrder = async () => {
+    console.log("Order saved!");
+    // const today = new Date();
+    // const formData = {
+    //   orderDate: today.toDateString(),
+    //   orderTime: today.toLocaleTimeString(),
+    //   orderAmount: cartTotalAmount,
+    //   orderStatus: "Order Placed...",
+    //   cartItems,
+    //   shippingAddress,
+    //   paymentMethod,
+    //   coupon: coupon != null ? coupon : { name: "nil" },
+    // };
+    // console.log(formData);
+    // dispatch(createOrder(formData));
+    // navigate("/checkout-success");
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setMessage(null)
 
     if (!stripe || !elements) {
-      // Stripe.js hasn't yet loaded.
-      // Make sure to disable form submission until Stripe.js has loaded.
       return;
     }
 
     setIsLoading(true);
 
-    const { error } = await stripe.confirmPayment({
-      elements,
-      confirmParams: {
-        // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000/complete",
-      },
-    });
-
-    // This point will only be reached if there is an immediate error when
-    // confirming the payment. Otherwise, your customer will be redirected to
-    // your `return_url`. For some payment methods like iDEAL, your customer will
-    // be redirected to an intermediate site first to authorize the payment, then
-    // redirected to the `return_url`.
-    if (error.type === "card_error" || error.type === "validation_error") {
-      setMessage(error.message);
-    } else {
-      setMessage("An unexpected error occurred.");
-    }
+    await stripe.confirmPayment({
+        elements, 
+        confirmParams: {
+            return_url: `${import.meta.env.VITE_REACT_APP_FRENTEND_URL}/checkout-success`,
+        },
+        redirect:"if_required"
+    })
+    .then((result) => {
+        // ok - paymentIntent // bad - error
+        if (result.error) {
+          toast.error(result.error.message);
+          setMessage(result.error.message);
+          return;
+        }
+        if (result.paymentIntent) {
+          if (result.paymentIntent.status === "succeeded") {
+            setIsLoading(false);
+            toast.success("Payment successful");
+            saveOrder();
+          }
+        }
+      });
 
     setIsLoading(false);
   };
@@ -52,24 +90,44 @@ export default function CheckoutForm({dpmCheckerLink}) {
 
   return (
     <>
-      <form id="payment-form" onSubmit={handleSubmit}>
-
-        <PaymentElement id="payment-element" options={paymentElementOptions} />
-        <button disabled={isLoading || !stripe || !elements} id="submit">
-          <span id="button-text">
-            {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
-          </span>
-        </button>
-        {/* Show any error or success messages */}
-        {message && <div id="payment-message">{message}</div>}
-      </form>
-      {/* [DEV]: Display dynamic payment methods annotation and integration checker */}
-      <div id="dpm-annotation">
-        <p>
-          Payment methods are dynamically displayed based on customer location, order amount, and currency.&nbsp;
-          <a href={dpmCheckerLink} target="_blank" rel="noopener noreferrer" id="dpm-integration-checker">Preview payment methods by transaction</a>
-        </p>
+    <section>
+      <div className={`container ${styles.checkout}`}>
+        <h2>Checkout</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <Card cardClass={styles.card}>
+              <CheckoutSummary />
+            </Card>
+          </div>
+          <div>
+            <Card cardClass={`${styles.card} ${styles.pay}`}>
+              <h3>Stripe Checkout</h3>
+              <PaymentElement id={styles["payment-element"]} />
+              <button
+                disabled={isLoading || !stripe || !elements}
+                id="submit"
+                className={styles.button}
+              >
+                <span id="button-text">
+                  {isLoading ? (
+                    // <img
+                    //   src={spinnerImg}
+                    //   alt="Loading..."
+                    //   style={{ width: "20px" }}
+                    // />
+                    <Spinner />
+                  ) : (
+                    "Pay now"
+                  )}
+                </span>
+              </button>
+              {/* Show any error or success messages */}
+              {message && <div id={styles["payment-message"]}>{message}</div>}
+            </Card>
+          </div>
+        </form>
       </div>
+    </section>
     </>
   );
 }
