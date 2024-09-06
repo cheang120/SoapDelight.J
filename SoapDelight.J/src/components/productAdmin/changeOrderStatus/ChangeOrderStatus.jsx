31f5a828e433863dc822ed20f441154741b96bc5
cheang120 <babycode.2023@gmail.com>
2024-09-06 22:44:18 +0800

    change order status

--- SoapDelight.J/src/components/productAdmin/changeOrderStatus/ChangeOrderStatus.jsx
+++ SoapDelight.J/src/components/productAdmin/changeOrderStatus/ChangeOrderStatus.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
-import "./ChangeOrderStatus.module.scss"
+import React, { useState } from 'react'
+import styles from "./ChangeOrderStatus.module.scss";
+import { Spinner } from "../../Loader";
+import { useDispatch, useSelector } from "react-redux";
+import Card from "../../card/Card";
+import { useNavigate, useParams } from 'react-router-dom'
+import { updateOrderStatus } from '../../../redux/features/order/OrderSlice';
+
+
+
 
 const ChangeOrderStatus = () => {
+    const {id} = useParams()
+    // console.log(id);
+    const [status, setStatus] = useState("")
+    const { isLoading } = useSelector((state) => state.order)
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const updateOrder = async(e, id) => {
+        e.preventDefault();
+        const formData = {
+          orderStatus: status,
+        };
+        // console.log(formData);
+        await dispatch(updateOrderStatus({ id, formData }));
+        
+    }
   return (
-    <div>
-      
-    </div>
+    <>
+        {isLoading && <Spinner />}
+        <div className={styles.status}>
+            <Card cardClass={styles.card}>
+            <h4>Update Status</h4>
+            <form onSubmit={(e) => updateOrder(e, id)}>
+                <span>
+                <select
+                    value={status}
+                    onChange={(e) => setStatus(e.target.value)}
+                >
+                    <option value="" disabled>
+                    -- Choose one --
+                    </option>
+                    <option value="Order Placed...">Order Placed...</option>
+                    <option value="Processing...">Processing...</option>
+                    <option value="Shipped...">Shipped...</option>
+                    <option value="Delivered">Delivered</option>
+                </select>
+                </span>
+                <span>
+                    <button type="submit" className="--btn --btn-primary">
+                        Update Status
+                    </button>
+                </span>
+            </form>
+            </Card>
+        </div>
+    </>
+
   )
 }
 
