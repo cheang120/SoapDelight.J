4cfc4ef4f454eef0b64bae0694d09b50819cb6f2
cheang120 <babycode.2023@gmail.com>
2024-09-14 22:37:20 +0800

    footer

--- SoapDelight.J/src/components/Footer.jsx
+++ SoapDelight.J/src/components/Footer.jsx
@@ -5,18 +5,20 @@ export default function FooterCom() {
   return (
     <Footer container className='border border-t-8 border-teal-500'>
       <div className='w-full max-w-7xl mx-auto'>
-        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
-          <div className='mt-5'>
+        <div className='grid w-full h-full justify-between sm:flex md:grid-cols-1'>
+          <div className='h-[5rem] flex items-center w-1/3 justify-center'>
             <Link
               to='/'
-              className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
+              className='flex whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
             >
-              <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+              <span className='px-2 py-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
                 Soapdelight.J
               </span>
             </Link>
           </div>
-          <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
+          {/* <div 
+            className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'
+          >
             <div>
               <Footer.Title title='About' />
               <Footer.LinkGroup col>
@@ -56,23 +58,27 @@ export default function FooterCom() {
                 <Footer.Link href='#'>Terms &amp; Conditions</Footer.Link>
               </Footer.LinkGroup>
             </div>
+          </div> */}
+          <div className='w-1/3 flex items-center justify-center'>
+            <Footer.Copyright
+              href='#'
+              by="babycode"
+              year={new Date().getFullYear()}
+            />
+          </div>
+
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center w-1/3 items-center">
+            <Footer.Icon href='#' icon={BsFacebook}/>
+            {/* <Footer.Icon href='#' icon={BsInstagram}/> */}
+            {/* <Footer.Icon href='#' icon={BsTwitter}/> */}
+            {/* <Footer.Icon href='https://github.com/sahandghavidel' icon={BsGithub}/> */}
+            {/* <Footer.Icon href='#' icon={BsDribbble}/> */}
           </div>
         </div>
-        <Footer.Divider />
+        {/* <Footer.Divider /> */}
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
-          <Footer.Copyright
-            href='#'
-            by="Sahand's blog"
-            year={new Date().getFullYear()}
-          />
-          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
-            <Footer.Icon href='#' icon={BsFacebook}/>
-            <Footer.Icon href='#' icon={BsInstagram}/>
-            <Footer.Icon href='#' icon={BsTwitter}/>
-            <Footer.Icon href='https://github.com/sahandghavidel' icon={BsGithub}/>
-            <Footer.Icon href='#' icon={BsDribbble}/>
 
-          </div>
+
         </div>
       </div>
     </Footer>
