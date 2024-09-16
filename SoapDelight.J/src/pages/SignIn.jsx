a374f8d618065d947441442152ee182f47e7eecc
cheang120 <babycode.2023@gmail.com>
2024-09-16 10:23:29 +0800

    //

--- SoapDelight.J/src/pages/SignIn.jsx
+++ SoapDelight.J/src/pages/SignIn.jsx
@@ -119,7 +119,7 @@ const [uCase, setUCase] = useState(false)
       // setLoading(true);
       // setErrorMessage(null);
       dispatch(signInStart())
-      const res = await fetch('/api/auth/signin', {
+      const res = await fetch('https://soapdelight-j.store/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
